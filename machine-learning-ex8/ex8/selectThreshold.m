3c00080675fd7f17ba39a828b44a089b8a792484
jeongwhanchoi <jeongwhan.choi@icloud.com>
2017-07-16 10:45:00 +0900

    Selecting the threshold, epsilon

--- machine-learning-ex8/ex8/selectThreshold.m
+++ machine-learning-ex8/ex8/selectThreshold.m
@@ -23,17 +23,16 @@ for epsilon = min(pval):stepsize:max(pval)
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
 
-
-
-
-
-
-
-
-
-
-
-
+    cvPrediction = (pval < epsilon);
+    
+    tp = sum((yval==1) & (cvPrediction==1));
+    fp = sum((yval==0) & (cvPrediction==1));
+    fn = sum((yval==1) & (cvPrediction==0));
+    
+    prec = tp / (tp+fp);
+    rec = tp / (tp+fn);
+    
+    F1 = (2*prec*rec) / (prec+rec);
 
     % =============================================================
 
